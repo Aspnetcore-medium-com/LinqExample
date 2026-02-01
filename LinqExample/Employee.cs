417fa73a1e1f983ba94e306ff68fd7f5ce1031ee
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-02-01 21:16:52 +0000

    Add project files.

---
+++ LinqExample/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Job { get; set; } = "";
+        public string City { get; set; } = "";
+    }
+}
---
+++ LinqExample/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinqExample;
+
+Console.WriteLine("Hello, World!");
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee { Id = 1, Name = "A", Job = "Designer", City = "New York" },
+    new Employee { Id = 2, Name = "B", Job = "Analyst", City = "Boston" },
+    new Employee { Id = 3, Name = "C", Job = "Developer", City = "Newark" },
+    new Employee { Id = 4, Name = "D", Job = "Manager", City = "Boston" },
+    new Employee { Id = 5, Name = "E", Job = "Manager", City = "New York" }
+};
